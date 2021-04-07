0fb716490f2182dca55c84a4cf4b7d6eb2d4e448
Tạ Quang Trung <76809187+taquangtrung115@users.noreply.github.com>
2021-04-07 12:05:54 +0700

    áds

--- app/src/main/java/com/example/appcooking/MainActivity.java
+++ app/src/main/java/com/example/appcooking/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.appcooking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
---
+++ app/src/main/java/com/example/appcooking/loadmain.java
@@ -0,0 +1,24 @@
+package com.example.appcooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class loadmain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loadmain);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(loadmain.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
